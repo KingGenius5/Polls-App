2e36494a92946153e1e4f2b0900362bed342e04a
Muhammad Tifak <moht20009@gmail.com>
2020-02-09 23:48:49 -0800

    Writing more views

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,33 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
+from django.views import View  # Import the View parent class
+from .models import Question
+from django.template import loader
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
+
+class ShowTimeView(View):  # Create a view class
+
+    # Change the function-based view to be called get and add the self param
+    def get(self, request):
+        now = datetime.now()
+        html = "<html><body>It is now {}</body></html>".format(now)
+        return HttpResponse(html)
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
